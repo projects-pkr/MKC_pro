4625b7deb377ff2936925ba0b69cd6d548f1ca9d
projects-pkr <rathorepradeepkumar16@gmail.com>
2024-05-02 15:09:34 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import './App.css'
- import Book_session from './Components/book_session/Book_session'
- import Our_menters from './Components/our-menters/Our_menter'
-import Our_performer from './Components/our performer/our_performer'
-import Footer from './Components/footer/Footer'
-import Keypoints from './Components/keypoints/Keypoints'
-import Banner from './Components/homepage/homepage'
-import Navbar from './Components/header/header'
-import Signup from './Components/signup/Signup'
+import React from 'react';
+import './App.css';
+import Navbar from './Components/header/header';
+import Banner from './Components/homepage/homepage';
+import Book_session from './Components/book_session/Book_session';
+import Our_menters from './Components/our-menters/Our_menter';
+import Signup from './Components/signup/Signup';
+import Our_performer from './Components/our performer/our_performer';
+import Keypoints from './Components/keypoints/Keypoints';
+import Footer from './Components/footer/Footer';
 function App() {
   return (
 
     <>
-    <Navbar/>
-    <Banner/>
-<Book_session/>
-<Our_menters/>
-<Keypoints/>
-<Signup/>
-<Our_performer/>
-      <Footer/>
+      <Navbar />
+      <Banner />
+      <Book_session />
+      <Our_menters />
+      <Signup />
+      <Keypoints />
+      <Our_performer />
+      <Footer />
     </>
   )
 }
